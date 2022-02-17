35d060905dd5a913d64120f08645e87d3e9d8363
sabarishiva <sabarisiva48@gmail.com>
2022-02-17 12:12:28 -0800

    salarycomp screen completed

---
+++ src/components/salarycomponent/salary/salary.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+import "./salary.css";
+
+const Salaryscreen = ()=>{
+  return (
+    <div className='salaryContainer' >
+      <div className='salarySubContainer' >
+       <div className='logo'>
+     
+         <bold>CRUD OPERATION</bold>
+        
+       </div>
+       <div>
+       <div className='Content1'><h1>Configure your employee's salary structure.</h1></div>
+           <div className='Content2'>Select the necessary components to be included in your employee's salary and preview the payslip.</div>
+       </div>
+       
+       
+   </div>
+    </div>
+  )
+}
+
+export default Salaryscreen;
